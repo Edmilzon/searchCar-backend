a9b4445d148fde58ec6ce14862de53c3505c3879
Edmilzon <edmilzon.luna@gmail.com>
2025-05-09 20:47:56 -0400

    insert reserva correction

--- src/modules/searchCar/searchCar.service.js
+++ src/modules/searchCar/searchCar.service.js
@@ -59,7 +59,7 @@ const findAll = async () => {
             },
           },
         },
-        reservas:{
+        reserva:{
           select: {
             fecha_inicio: true ,
             fecha_fin : true,
