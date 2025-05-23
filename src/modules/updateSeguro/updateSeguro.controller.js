f2b22a1e75963df980b9adaf06a36df000278bfe
Maria Belen Quisbert Fernandez <maria.belen.quis@gmail.com>
2025-05-23 19:53:06 -0400

    endpoind de modificar realizado

--- src/modules/updateSeguro/updateSeguro.controller.js
+++ src/modules/updateSeguro/updateSeguro.controller.js
@@ -3,20 +3,22 @@ const router = express.Router();
 const { updateSeguro } = require('./updateSeguro.service');
 
 router.put('/:id', async (req, res) => {
-  const { id } = req.params;
+  const id = req.params.id;
   const data = req.body;
-
+  console.log(id);
   try {
+    
     const updatedRecord = await updateSeguro(id, data);
 
     if (!updatedRecord) {
       return res.status(404).json({ error: 'Seguro no encontrado.' });
     }
-
+  
     res.status(200).json(updatedRecord);
   } catch (error) {
     console.error('Error al actualizar el seguro:', error.message);
     res.status(500).json({ error: 'Error al actualizar el seguro y sus tipos asociados.' });
+    //console.error(error);
   }
 });
 
