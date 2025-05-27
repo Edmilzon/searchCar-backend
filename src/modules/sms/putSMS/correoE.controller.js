720f25bd86abf0f864da5325b46c827b8d3d3e29
Edmilzon <edmilzon.luna@gmail.com>
2025-05-26 23:10:25 -0400

    modificate update

--- src/modules/sms/putSMS/correoE.controller.js
+++ src/modules/sms/putSMS/correoE.controller.js
@@ -9,6 +9,9 @@ correoE.put('/:id', async(req , res) =>{
     const {estado} = req.body;
 
     try {
+        if (estado !== null) {
+            return res.status(400).json({ error: 'El estado ya fue actualizado y no puede ser modificado nuevamente.' });
+        }
         const updateNotificacion = await correoEService.updateEstado(id, estado);
         res.json(updateNotificacion)
     } catch (error) {
@@ -21,6 +24,9 @@ correoECorreo.get('/:id', async (req, res) => {
     const id = req.params.id;
     const estado = req.query.estado === 'true';
     try {
+        if (estado !== null) {
+            return res.status(400).send('<h2>El estado ya fue actualizado y no puede ser modificado nuevamente.</h2>');
+        }
         const updateNotificacion = await correoEService.updateEstado(id, estado);
         res.send(`<h2>Estado actualizado correctamente</h2>`);
     } catch (error) {
